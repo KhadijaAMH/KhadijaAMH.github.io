6b28f1a643e424f4105a524d2ad25d795e7202f4
Khadija Mohamad Haneefa <diju19599@gmail.com>
2020-12-30 14:39:47 +0530

    Update sw.js

--- getting-started-pwa-master/sw.js
+++ getting-started-pwa-master/sw.js
@@ -1,10 +1,11 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/master.css", "./images/logo192.png"]);
+            return cache.addAll(["./", "./src/master.css", "./images/logo192maskable.png"]);
         })
     );
 });
+
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
