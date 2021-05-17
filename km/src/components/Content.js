d071595631c2b264a9fb4ba837bc80515ab1c944
Khadija Mohamad Haneefa <diju19599@gmail.com>
2021-05-17 15:31:34 +0530

    removed KM

--- km/src/components/Content.js
+++
@@ -1,9 +0,0 @@
-const Contents = () => {
-    return (
-        <div className="Contents">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eaque quis minus rerum aspernatur suscipit saepe iusto omnis expedita consequatur?
-        </div>
-    )
-}
-
-export default Contents
